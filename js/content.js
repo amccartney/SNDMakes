080c9ffcf8288d89c68d16e79e0b14959ee2a93e
amccartney <allison.n.mccartney@gmail.com>
2016-04-05 15:19:21 -0700

    cleaning up, adding comments

--- js/content.js
+++ js/content.js
@@ -1,23 +1,7 @@
 $(document).ready(function() {
     // $(document).on('selectionchange', function(e) {
     //     var selection = window.getSelection().toString() || "";
-
-    //     var openIcon = $("<div id='open'></div>").css({
-    //                 width: "100px",
-    //                 height: "100px",
-    //                 backgroundColor: "blue",
-    //                 cursor: "pointer",
-    //                 position: "fixed",
-    //                 top: "10px",
-    //                 right: "10px", 
-    //                 zIndex: "9999999999999",
-    //             });
-    //     $('#open').remove();
-    //     openIcon.prependTo("body");
-    //     // $('<iframe id="someId"/></iframe>').appendTo('body');
     // });
-
-    
 
 
     // Icon to indicate where to open the iframe
@@ -50,17 +34,17 @@ $(document).ready(function() {
                 });
 
 
-    var iframe = $('<iframe id="form-iframe"/></iframe>').css({
-            width: "320px",
-            height: "1000px",
-            cursor: "pointer",
-            position: "fixed",
-            borderLeft: "3px solid Gainsboro",
-            backgroundColor: "white",
-            top: "0px",
-            right: "0px", 
-            zIndex: "99999999999"
-        });
+    var iframe = $('<iframe id="form-iframe" /></iframe>').css({
+                    width: "320px",
+                    height: "1000px",
+                    cursor: "pointer",
+                    position: "fixed",
+                    borderLeft: "3px solid Gainsboro",
+                    backgroundColor: "white",
+                    top: "0px",
+                    right: "0px", 
+                    zIndex: "99999999999"
+                });
 
     // Wrap the existing page elements with a new element whose width will change when the iframe is opened
     var bodyWrap = $('body').children().wrap('<div class="push-over" style="margin: 0;  padding: 0; border-radius: 0;"></div>');
@@ -71,6 +55,9 @@ $(document).ready(function() {
     // Create a variable that will store the width of the main page when the iframe is open
     var closedDocWidth = $(document).width() - 320;
 
+    
+
+    // For selecting highlighted text from the user
     // var response = $(document).on('selectionchange', function(e) {
     //     var selection = window.getSelection().toString() || "";
     // });
@@ -84,7 +71,7 @@ $(document).ready(function() {
         $('#form-iframe').remove();
 
         // Prepend the iframe to the body
-        iframe.prependTo('body').contents().find('body').append('test');
+        iframe.prependTo('body').attr('src', 'http://localhost:3000/') //.contents().find('body').append('test');
 
         // Prepend an 'x' button to allow people to close the iframe
         exitIcon.prependTo('body');
@@ -93,16 +80,20 @@ $(document).ready(function() {
     });
 
 
+
     $(document).on('click', '#close', function(e){
+
+        // Remove iframe from side of page
         $('#form-iframe').remove();
+
+        // Remove the close button
         $('#close').remove();
+
+        // Restore width of original page elements
         $('.push-over').css("width", "100%");
+
+        // Put the open icon back at the top of the page
         openIcon.prependTo("body");
     });
-
-    // var iframe = $('#form-iframe')
-    // iframe.ready(function() {
-    //     iframe.contents().find("body").append('<p>Test</p>');
-    // });
 
 });
